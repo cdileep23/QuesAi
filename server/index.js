1aa2e90b3a0749ada45f69bdc15e5ce887d021aa
dileep <cdileepkumar22@gmail.com>
2025-06-27 11:59:54 +0530

    server cos multiple

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://ques-ai-one.vercel.app"],
     credentials: true,
   })
 );
