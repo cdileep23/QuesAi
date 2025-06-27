f2e55efadc7b1a256495cfbfffe5cfa53aa758f6
dileep <cdileepkumar22@gmail.com>
2025-06-27 12:12:36 +0530

    cookie issue

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -96,7 +96,7 @@ export const Login = async (req, res) => {
       .cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", 
         maxAge: 2 * 24 * 60 * 60 * 1000,
       })
       .json({
