31fb8432116f43b2f52ffdd3a5310abef0a858a7
dileep <cdileepkumar22@gmail.com>
2025-06-27 13:55:31 +0530

    cookie removal in prod

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -139,11 +139,12 @@ export const UserProfile = async (req, res) => {
 
 export const Logout = async (req, res) => {
   try {
-    res.clearCookie("token", {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-    });
+   res.clearCookie("token", {
+     httpOnly: true,
+     secure: process.env.NODE_ENV === "production",
+     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+     path: "/", 
+   });
 
     return res.status(200).json({
       success: true,
