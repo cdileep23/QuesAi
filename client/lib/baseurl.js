93205b7bba69293ed4776b0d71e5a0d4686a72ed
dileep <cdileepkumar22@gmail.com>
2025-06-27 11:55:59 +0530

    client dynamic url added

--- client/lib/baseurl.js
+++ client/lib/baseurl.js
@@ -1,1 +1,4 @@
-export const BASE_URL = "http://localhost:4545/api/v1";
+export const BASE_URL =
+  location.hostname === "localhost"
+    ? "http://localhost:4545/api/v1"
+    : "https://quesai-dk5a.onrender.com/api/v1";
